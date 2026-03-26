343b585d512a54f793e45884a4dd9640e3766096
Gnana Sagar <132949052+SadasivuniGnanasagar@users.noreply.github.com>
2026-03-26 16:19:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/users', userRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
